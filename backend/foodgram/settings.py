80d115e4dc99596651d7871c0fee64d988954fe6
ArtemL <sntchweb@yandex.ru>
2023-08-14 18:10:04 +0400

    test workflow

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -62,13 +62,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'foodgram.wsgi.application'
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
