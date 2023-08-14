5b420c571bd99da5d81033cd0153dcfb86d701e4
ArtemL <sntchweb@yandex.ru>
2023-08-14 18:53:34 +0400

    oh isort

--- backend/api/filters.py
+++ backend/api/filters.py
@@ -1,7 +1,6 @@
 from django_filters.rest_framework import (FilterSet,
                                            ModelMultipleChoiceFilter,
                                            NumberFilter)
-
 from recipes.models import Recipe, Tag
 
 
